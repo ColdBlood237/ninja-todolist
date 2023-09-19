25457d07d53c31a46300d2af6138c7b51cc929be
ryan.petnga <ryan.petnga@mykritek.eu>
2023-09-19 16:48:05 +0200

    Validate todo input to be longer than 3 chars

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Alert } from "react-native";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -15,11 +16,23 @@ export default function App() {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.key !== key));
   };
 
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* to form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
